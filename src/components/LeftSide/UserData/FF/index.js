32b66ee5e23d9c2b009645e0bd730b7b03d0f4ff
SudipTimalsina <timalsinasudip11@gmail.com>
2023-08-16 08:46:45 +0545

    Updating the files in Managed order

---
+++ src/components/LeftSide/UserData/FF/index.js
@@ -0,0 +1,20 @@
+
+import Follower from "./Follower";
+import Following from "./Following";
+
+function FF (props) {
+    return (
+      <ul className="user-fw-status">
+        <Follower user={props.user} />
+        <Following user={props.user} />
+        <li>
+          <a href="./my-profile.html" title="">
+            View Profile
+          </a>
+        </li>
+      </ul>
+    );
+  
+}
+
+export default FF;
