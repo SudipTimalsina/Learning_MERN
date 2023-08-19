3e9bab99afe0fea8e7087c747035c6c06bed6a23
SudipTimalsina <timalsinasudip11@gmail.com>
2023-08-19 19:39:33 +0545

    Final Changes

--- src/components/Common/Header/Logo.js
+++ src/components/Common/Header/Logo.js
@@ -1,11 +1,11 @@
-const Logo = () =>{
-    return (
-        <div className="logo">
-        <a href="./index.html" title="">
-          <img src="./images/lec.png" alt="" />
-        </a>
-      </div>
-    );
+const Logo = () => {
+  return (
+    <div className="logo">
+      <a href="/#" title="">
+        <img src="./images/lec.png" alt="" />
+      </a>
+    </div>
+  );
 };
 
 export default Logo;
--- src/components/Common/Header/index.js
+++ src/components/Common/Header/index.js
@@ -1,23 +1,23 @@
 import Logo from "./Logo";
 import SearchBar from "./SearchBar";
-import UserAccount from "./UserAccount";
 import Nav from "./Nav";
 import Menu from "./Menu";
+import UserAccount from "./UserAccount";
 
 const Header = (props) => {
-    return (
-      <header>
-        <div className="container">
-          <div className="header-data">
-           <Logo/>
-            <SearchBar/>
-            <Nav/>
-            <Menu/>
-            <UserAccount user={props.user}/>
-          </div>
+  return (
+    <header>
+      <div className="container">
+        <div className="header-data">
+          <Logo />
+          <SearchBar handleSearch={props.handleSearch} />
+          <Nav />
+          <Menu />
+          <UserAccount user={props.user} />
         </div>
-      </header>
-    );
+      </div>
+    </header>
+  );
 };
 
 export default Header;
